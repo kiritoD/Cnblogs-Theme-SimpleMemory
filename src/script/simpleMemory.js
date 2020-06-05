466a892beeed485987a2af4dfe7fc48ee5de34ca
pc <1169394593@qq.com>
2020-06-06 00:34:04 +0800

    debugger

--- src/script/simpleMemory.js
+++ src/script/simpleMemory.js
@@ -3,7 +3,7 @@
  * https://www.cnblogs.com/bndong/
  * @author: BNDong, dbnuo@foxmail.com
  **/
-debugger;
+
 if (initCheck()) {
 
     var sidebarHtml =
@@ -339,6 +339,7 @@ function init() {
     var url = window.location.href, tmp = [];
     tmp = url.split("/");
     var user = tmp[3];
+    debugger;
     var navListHtml = '<li><a href="https://www.cnblogs.com/' + user + '/" target="_self">首页</a></li>' +
         '<li><a href="https://msg.cnblogs.com/send/' + user + '" target="_blank">联系</a></li>' +
         '<li><a href="https://www.cnblogs.com/' + user + '/rss" target="_blank">订阅</a></li>' +
